ece15e13c62c8593d19a832455bfdea4e1121921
BVishal-Geek <bakshiv193@gmail.com>
2023-12-02 18:09:58 -0500

    K-Means Clustering

--- code/Visualization.py
+++ code/Visualization.py
@@ -7,7 +7,8 @@ import seaborn as sns
 from imblearn.over_sampling import SMOTE,SMOTENC
 from imblearn.pipeline import make_pipeline
 from pyspark.ml.feature import VectorAssembler
-
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
 
 class Visualization:
 
@@ -27,13 +28,6 @@ class Visualization:
         plt.ylabel('Count')
         plt.show()
     
-    def histPlot(self):
-        sns.histplot(data=self._artist, x = "num_rank_cat")
-        plt.title('Hist Plot')
-        plt.xlabel('No.of Times Ranked')
-        plt.ylabel('Count')
-        plt.show()
-
     def transformingDataSql(self):
 
         # Register DataFrame as a temporary table
@@ -41,7 +35,7 @@ class Visualization:
 
         session = cr.transformData()
         # Perform the SQL query
-        result_df = session.sql("""
+        self.result_df = session.sql("""
             SELECT
                 artist,
                 first(total_streams) as total_streams,
@@ -56,17 +50,49 @@ class Visualization:
             GROUP BY
                 artist
         """)
-
+        
         # Show the result
-        return result_df.show()
+        return self.result_df.show()
 
+    def duplicateRecords(self):
+        duplicateRecords = self.result_df.exceptAll(self.result_df.dropDuplicates(['artist']))
+        return duplicateRecords.count()
+    
+    def kMeansClustering(self):
         
+        numerical_columns = self.result_df.columns[1:]
+        # numerical_columns = ["artist","total_streams","top10","top20", "top50", "top100", "top200"]
+
+        # Display the first few rows of the DataFrame with numerical columns
+        self.result_df.select(numerical_columns).show(5)
+        # Assemble features into a single vector column
+        assembler = VectorAssembler(inputCols=numerical_columns, outputCol="features")
+        assembled_df = assembler.transform(self.result_df)
         
-        
+        k_cluster = 3 
+        kmeans = KMeans(featuresCol="features", predictionCol="cluster", k=k_cluster, seed=123)
+
+        kmeans_model = kmeans.fit(assembled_df)
+
+        # Get cluster assignments for each data point
+        clusteringModel = kmeans_model.transform(assembled_df)
+
+        #Checking the counts of the clusters:
+        print(clusteringModel.groupBy("cluster").count().show())
+
+        evaluator = ClusteringEvaluator(predictionCol="cluster") 
+        silhouette_score = evaluator.evaluate(clusteringModel)
+        print(f"Silhouette Score: {silhouette_score}")
+
+        self.result_df.show()
 
 if __name__ == "__main__":
     data = Visualization()
     data.getData()
-    #data.countPlot()
+    data.countPlot()
     data.transformingDataSql()
+    #data.duplicateRecords()
+    data.kMeansClustering()
     
+
+# %%
