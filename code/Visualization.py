94ca14a89c49491f83305364cf9b450f4bd459a9
BVishal-Geek <bakshiv193@gmail.com>
2023-12-02 11:12:46 -0500

    Transforming data

--- code/Visualization.py
+++ code/Visualization.py
@@ -1,8 +1,10 @@
+#%%
 import code_runner as cr
-from pyspark.sql import SparkSession
+import createSession as cs
+from pyspark.sql import SparkSession as spark
 import matplotlib.pyplot as plt
 import seaborn as sns
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE,SMOTENC
 from imblearn.pipeline import make_pipeline
 from pyspark.ml.feature import VectorAssembler
 
@@ -17,18 +19,54 @@ class Visualization:
         self.top200DF = cr.readTop200Cat()
         return self.artistDF, self.top200DF
 
-    def plot(self):
-        self.data = self.artistDF.toPandas()
-        sns.countplot(data=self.data, x="rank_cat")
+    def countPlot(self):
+        self._artist = self.artistDF.toPandas()
+        sns.countplot(data=self._artist, x="rank_cat")
         plt.title('Count Plot')
         plt.xlabel('Rank Category')
         plt.ylabel('Count')
         plt.show()
+    
+    def histPlot(self):
+        sns.histplot(data=self._artist, x = "num_rank_cat")
+        plt.title('Hist Plot')
+        plt.xlabel('No.of Times Ranked')
+        plt.ylabel('Count')
+        plt.show()
+
+    def transformingDataSql(self):
+
+        # Register DataFrame as a temporary table
+        self.artistDF.createOrReplaceTempView("my_table")
+
+        session = cr.transformData()
+        # Perform the SQL query
+        result_df = session.sql("""
+            SELECT
+                artist,
+                first(total_streams) as total_streams,
+                SUM(CASE WHEN rank_cat = 'top10' THEN num_rank_cat ELSE 0 END) AS top10,
+                SUM(CASE WHEN rank_cat = 'top20' THEN num_rank_cat ELSE 0 END) AS top20, 
+                SUM(CASE WHEN rank_cat = 'top50' THEN num_rank_cat ELSE 0 END) AS top50,                      
+                SUM(CASE WHEN rank_cat = 'top100' THEN num_rank_cat ELSE 0 END) AS top100,
+                SUM(CASE WHEN rank_cat = 'top200' THEN num_rank_cat ELSE 0 END) AS top200
+                
+            FROM
+                my_table
+            GROUP BY
+                artist
+        """)
+
+        # Show the result
+        return result_df.show()
+
+        
+        
+        
 
 if __name__ == "__main__":
     data = Visualization()
     data.getData()
-    data.plot()
-
-    # Uncomment the following lines if you want to perform SMOTE analysis
-    # balanced_features, balanced_labels = data.balancingData()
+    #data.countPlot()
+    data.transformingDataSql()
+    
